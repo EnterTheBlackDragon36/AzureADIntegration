878ed59c8138445d7756fa402b4afcbb1fbe659d
GPS Tracking Demo <entertheblackdragon@gmail.com>
2024-08-14 18:44:17 -0500

    Add project files.

---
+++ Areas/Identity/Data/AzureADIntegrationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureADIntegration.Data;
+
+public class AzureADIntegrationContext : IdentityDbContext<IdentityUser>
+{
+    public AzureADIntegrationContext(DbContextOptions<AzureADIntegrationContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,26 @@
+﻿using AzureADIntegration.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AzureADIntegration.Controllers
+{
+    [Authorize]
+    public class SecurityController : Controller
+    {
+        public static IConfiguration Configuration { get; set; }
+        
+        public SecurityController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ActionResult Index()
+        {
+
+            
+            return View("~/Views/Security/Protected.cshtml");
+        }
+    }
+}
